dc9afa053b4ba16415f8eda89db90a02ac19c0b5
Brandon Silvi <brandon.silvi@gmail.com>
2025-11-12 17:11:02 -0800

    grade validator created and tested with 1 valid and 2 invalid inputs.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-myGrades = [100, 100, 90, 73, 78, 94, 86];
+const myGrades = [100, 100, 90, 73, 78, 94, 86];
 
-myAverage = 0;
+let myAverage = 0;
 //variables should be declared by using let 
 for (let i = 0; i < myGrades.length; i++) {
 //i should start at 0 which is the first item in the array.
@@ -12,3 +12,21 @@ myAverage += myGrades[i];
 myAverage = myAverage / myGrades.length;
 
 console.log("My grade average is " + myAverage);
+////////////////////////////////////////////////// 
+console.log("Question2")
+function validateGrade(grade) {
+    if (typeof grade !== "number" || isNaN(grade)) {
+        throw new Error("Bad input: grade must be a number value.");
+    }
+    if (grade < 0) {
+        throw new Error("Bad input:grade cannot be less than 0")
+    }
+    if (grade > 100) {
+        throw new Error("Bad input:grade cannot be greater than 100")
+    }
+
+    console.log("Valid Grade:", grade);
+}
+validateGrade(50);
+validateGrade(C);
+validateGrade(749);
